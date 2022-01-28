de9d0dcf793a6d58780ca22901f7db183567b8ae
Kieran Lewis <kieranlewis1334@gmail.com>
2022-01-29 11:17:15 +1300

    fixed eslint not working nice with jest

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -2,5 +2,5 @@ const Ship = require('../modules/Ship');
 
 test('ship has the correct length when defining creating new ship', () => {
     const testShip = Ship(3);
-    expect(testShip.length).toBe(3);
+    expect(testShip.length).toBe(3); 
 })
