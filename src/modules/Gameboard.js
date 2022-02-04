d89b83388176132b7129cd81d04d74849d63b2c3
Kieran Lewis <kieranlewis1334@gmail.com>
2022-02-04 23:35:38 +1300

    creates an empty 10 x 10 grid in the gameboard factory

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,5 +1,20 @@
 const Gameboard = () => {
+    let board = createEmptyBoard();
 
+    function createEmptyBoard() {
+        let board = []
+        for(let i = 0; i < 10; i++) {
+            board.push(new Array(10).fill(''));
+        }
+
+        return board
+    }
+
+    return {
+        get board() {
+            return board;
+        }
+    }
 }
 
 export { Gameboard };
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,1 +1,10 @@
 import { Gameboard } from "../modules/Gameboard";
+//import { Ship } from "../modules/Ship";
+
+test('Board size should be a 10 x 10 grid', () => {
+    const testBoard = Gameboard();
+
+    for(let i = 0; i < 10; i++) {
+        expect(testBoard.board[i].length).toBe(10)
+    }
+})
