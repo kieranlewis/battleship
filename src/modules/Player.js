92d9c9b59ab226ec3f13b32ae72a4b573943e1da
Kieran Lewis <kieranlewis1334@gmail.com>
2022-02-07 00:43:21 +1300

    cpu ai will never repeat a coordinate it has already tried

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,6 +1,31 @@
 const Player = (name) => {
+    const _randomCoordsList = [];
+
     function attackEnemy(gameBoard, coords) {
         gameBoard.receiveAttack(coords);
+    }
+
+    function randomAttack(gameBoard) {
+        if(_randomCoordsList.length === 100) return;
+
+        let randomY, randomX;
+
+        do {
+            randomY = Math.floor(Math.random() * 10);
+            randomX = Math.floor(Math.random() * 10);
+        } while(_beenHit(randomY, randomX))
+
+        _randomCoordsList.push([randomY, randomX]);
+        gameBoard.receiveAttack([randomY, randomX]);
+    }
+
+    function _beenHit(y, x) {
+        for(let i = 0; i < _randomCoordsList.length; i++) {
+            if(_randomCoordsList[i][0] === y && _randomCoordsList[i][1] === x) {
+                return true
+            }
+        }
+        return false;
     }
 
     return {
@@ -8,7 +33,7 @@ const Player = (name) => {
             return name;
         },
         
-        attackEnemy
+        attackEnemy, randomAttack
     }
 }
 
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,12 +1,46 @@
 import { Gameboard } from "../modules/Gameboard";
 import { Ship } from "../modules/Ship";
-import { Player } from "../modules/Player";
+import { Player, ComputerAI } from "../modules/Player";
 
-test('players can attack the enemy gameboard', () => {
+test('players can attack the enemy gameboard (1)', () => {
     const testBoard = Gameboard();
     const p1 = Player('p1');
 
     p1.attackEnemy(testBoard, [0,0]);
-    console.log(testBoard.board);
+
     expect(testBoard.board[0][0]).toBe('M');
 }) 
+
+test('players can attack the enemy gameboard (2)', () => {
+    const testBoard = Gameboard();
+    const testShip = Ship(3);
+    const p1 = Player('p1');
+
+    testBoard.placeShip(testShip, [0,0], 'horizontal');
+    p1.attackEnemy(testBoard, [0,0]);
+
+    expect(testBoard.board[0][0]).toBe('H');
+}) 
+
+test('computer can attack the enemy with a random coordinate', () => {
+    const testBoard = Gameboard();
+    const cpu = Player('cpu1');
+
+    cpu.randomAttack(testBoard);
+
+    const test = testBoard.board.some(row => row.includes('M'));
+    expect(test).toBe(true);
+})
+
+test('computer should never attack the same coordinate again', () => {
+    const testBoard = Gameboard();
+    const cpu = Player('cpu1');
+
+    for(let i = 0; i < 100; i++) {
+        cpu.randomAttack(testBoard);
+    }
+    
+    console.log(testBoard.board);
+    const test = testBoard.board.every(row => row.every(value => value == 'M'));
+    expect(test).toBe(true);
+})
