f8f292d0ecd241c1354cfdc2c817c4fc910b41a8
Kieran Lewis <kieranlewis1334@gmail.com>
2022-02-10 16:57:11 +1300

    display a board to html page

--- src/game.js
+++ src/game.js
@@ -5,7 +5,8 @@ import { render } from "./modules/domManipulation";
 
 const game = (() => {
     function init() {
-        const testBoard = Gameboard();
+        const playerBoard = Gameboard();
+        const cpuBoard = Gameboard();
         const player = Player('Kieran');
         const cpu = Player('Computer');
     
@@ -13,8 +14,15 @@ const game = (() => {
         const playerShip2 = Ship(3);
         const cpuShip1 = Ship(2);
         const cpuShip2 = Ship(3);
-        
-        render(testBoard);
+
+        playerBoard.placeShip(playerShip1, [0,0], 'horizontal');
+        playerBoard.placeShip(playerShip2, [2,2], 'vertical');
+
+        cpuBoard.placeShip(cpuShip1, [0,0], 'horizontal');
+        cpuBoard.placeShip(cpuShip2, [2,2], 'vertical');
+
+        render(playerBoard);
+        //render(cpuBoard);
     }
 
     return {
--- src/modules/domManipulation.js
+++ src/modules/domManipulation.js
@@ -1,10 +1,15 @@
 function render(gameBoard) {
     const board = gameBoard.board;
-    const boardDiv = document.querySelector('.board');
+    console.log(board);
+    const boardDiv = document.querySelector('.player1-board');
 
     for(let i = 0; i < 10; i++) {
         for(let j = 0; j < 10; j++) {
-            console.log(board[i][j]);
+            const div = document.createElement('div');
+            div.setAttribute('class', 'grid-item');
+            div.innerText = board[i][j];
+            boardDiv.appendChild(div);
+            //console.log(board[i][j]);
         }
     }
 }
