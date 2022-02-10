94ed177c6b50895905635040279fe3782e1dc987
Kieran Lewis <kieranlewis1334@gmail.com>
2022-02-10 17:28:58 +1300

    added data attributes and event listener to board tiles

--- src/modules/domManipulation.js
+++ src/modules/domManipulation.js
@@ -1,15 +1,25 @@
 function render(gameBoard, player) {
     const board = gameBoard.board;
     const boardDiv = document.querySelector(`.player${player}-board`);
+    boardDiv.innerHTML = '';
 
     for(let i = 0; i < 10; i++) {
         for(let j = 0; j < 10; j++) {
             const div = document.createElement('div');
             div.setAttribute('class', 'grid-item');
+            div.setAttribute('data-coord', [i,j]);
+            div.setAttribute('data-player', player);
             div.innerText = board[i][j];
+
+            div.addEventListener('click', _attack)
+
             boardDiv.appendChild(div);
         }
     }
+}
+
+function _attack() {
+    console.log(this.getAttribute('data-coord'))
 }
 
 export { render }
